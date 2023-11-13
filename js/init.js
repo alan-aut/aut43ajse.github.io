78d4c3b76a444f238ead373058c31eb5451583ec
alan-aut <150398542+alan-aut@users.noreply.github.com>
2023-11-13 10:38:11 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,2 +1,13 @@
-# Autenticación
-Ejemplo de autenticación con Firebase y Google.
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDs8mRWk0H6znXZnFlyf3tAyrwCA7SJf_o",
+  authDomain: "aut43as.firebaseapp.com",
+  projectId: "aut43as",
+  storageBucket: "aut43as.appspot.com",
+  messagingSenderId: "291041859735",
+  appId: "1:291041859735:web:9b811a83cc6c900ec131d2",
+  measurementId: "G-Q57F5MX2J4"
+    });
+
